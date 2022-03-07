219018ad9ae29c42b0fc9a2f6514fa02e91b6b74
Grace Acton <gkacton@bobcat-127-209.bates.edu>
2022-03-07 10:33:04 -0500

    Initial commit, load packages

---
+++ SpatialData.R
@@ -0,0 +1,12 @@
+
+# Load Packages -----------------------------------------------------------
+
+library(tidyverse) ## For plotting and data wrangling.
+library(leaflet) ## For leaflet interactive maps
+library(sf) ## For spatial data
+library(RColorBrewer) ## For colour palettes
+library(htmltools) ## For html
+library(leafsync) ## For placing plots side by side
+library(kableExtra) ## Table  output (in slides)
+
+
